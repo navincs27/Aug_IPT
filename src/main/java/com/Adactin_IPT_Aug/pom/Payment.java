7526daad76e3282496550bee15e384f197788cb9
Dhakchayini <a.n.swathyraj@gmail.com>
2024-08-25 11:11:59 +0530

    paymentpage added

--- src/main/java/com/Adactin_IPT_Aug/pom/Payment.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/Payment.java
@@ -1,5 +1,9 @@
 package com.Adactin_IPT_Aug.pom;
 
 public interface Payment {
+	
+String firstname_name = "first_name";
+	
+	String lastname_name = "last_name";
 
 }
--- src/main/java/com/Adactin_IPT_Aug/pom/PaymentPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/PaymentPage.java
@@ -1,5 +1,20 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PaymentPage implements Payment{
 
+		
+
+	@FindBy(name = firstname_name)
+	private WebElement firstname;
+
+	@FindBy(name = lastname_name)
+	private WebElement lastname;
+	
+	public void print() {
+		System.out.println("This is Swathy");
+	}
+	
 }
