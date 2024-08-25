f5af90531ad7035413f2d37a8707d6f6cb7fb440
AshlynRose0405 <ashlynrose0405@gmail.com>
2024-08-25 11:03:37 +0530

    RoomType and NoOfRoom Elements added

--- src/main/java/com/Adactin_IPT_Aug/pom/SearchHotel.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/SearchHotel.java
@@ -2,4 +2,8 @@ package com.Adactin_IPT_Aug.pom;
 
 public interface SearchHotel {
 
+	String RoomType_id = "room_type";
+
+	String NoOfRooms_id = "room_nos";
+	
 }
--- src/main/java/com/Adactin_IPT_Aug/pom/SearchHotelPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/SearchHotelPage.java
@@ -1,5 +1,19 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class SearchHotelPage implements SearchHotel{
+	
+	@FindBy(id = RoomType_id)
+	private WebElement roomType;
+
+	@FindBy(id = NoOfRooms_id)
+	private WebElement noOfRooms;
+	
+	private void name() {
+
+		System.out.println("Rossie");
+	}
 
 }
