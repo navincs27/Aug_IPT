09a7d3e3c11f226f6d37315176bc37551c3dd4d3
vijayvidhyut <vijayvidhyut@gmail.com>
2024-08-25 11:56:47 +0530

    vijay

--- src/main/java/com/Adactin_IPT_Aug/pom/Payment.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/Payment.java
@@ -1,5 +1,8 @@
 package com.Adactin_IPT_Aug.pom;
 
 public interface Payment {
+	    String ccvnobtn_id="cc_cvv";
+	    String booknowbtn_id="book_now";
+	    
 
 }
--- src/main/java/com/Adactin_IPT_Aug/pom/PaymentPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/PaymentPage.java
@@ -1,5 +1,17 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PaymentPage implements Payment{
 
+	@FindBy(id=ccvnobtn_id)
+	private WebElement ccvno_btn;
+	
+	@FindBy(id=booknowbtn_id)
+	private WebElement booknow_btn;
+	
+	public void bookingHotel() {
+		System.out.println("booked");
+	}
 }
