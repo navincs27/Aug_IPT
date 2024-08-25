681e3bc8bec4a38c1eaeb662aea8c8e446a2baa5
malik <malikbasha915.com>
2024-08-25 11:01:10 +0530

    password added

--- src/main/java/com/Adactin_IPT_Aug/pom/Login.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/Login.java
@@ -1,5 +1,7 @@
 package com.Adactin_IPT_Aug.pom;
 
 public interface Login {
+	
+	String password_name= "password";
 
 }
--- src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
@@ -1,6 +1,19 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage implements Login{
+	
+	@FindBy(name= password_name)
+	private WebElement password;
+	
+	public void malikbasha() {
+		System.out.println("malik");
+
+	}
+	
+
 	
 	
 
