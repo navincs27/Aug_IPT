f6037de24da2a45ff68c2f7ef96e9ce3f65c0feb
DheepanVinothKumar <mvinothkumar07@gmail.com>
2024-08-25 11:01:15 +0530

    login page created

--- src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
@@ -1,7 +1,18 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage implements Login{
 	
+	@FindBy(id = User_id)
+	private WebElement username;
+
+
+public static void main(String args[]) {
+
+	System.out.println("Hello! Welcome");
 	
+}
 
 }
