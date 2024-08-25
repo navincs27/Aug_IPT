3a59793982a3964ffebaa7ad36c45b821328e7dd
LPriya1208 <rmtlpriya12@gmail.com>
2024-08-25 10:59:59 +0530

    PageFactory added

--- src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
+++ src/main/java/com/Adactin_IPT_Aug/pom/LoginPage.java
@@ -1,7 +1,24 @@
 package com.Adactin_IPT_Aug.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage implements Login{
 	
+	
+	
+	
+	
+	
+	
+	public void lakshmiPriya() {
+		System.out.println("Hello Lakshmi Priya");
+	}
+	
+	public AdactinLoginPOM(WebDriver driver) {
+		adactinDriver=driver;
+		PageFactory.initElements(driver,this);		
+	}
 	
 
 }
